95dacc0aa252aa25494da3030d058d8a729a476d
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-08-08 00:55:07 +0000

    first

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [name, setName] = useState("")
+	const [age, setAge] = useState(0)
+	const [result, setResult] = useState(false)
+	function validateSubmission() {
+		if (name == " ") {
+			alert("name is needed!!")
+		}
+		fetchAge();
+
+		function fetchAge() {
+			fetch("https://api.agify.io/?name=" + name)
+				.then(response => response.json())
+				.then(data => {
+					setName(data.name);
+					setAge(data.age)
+				})
+		}
+
+		useEffect(() => {
+			fetchAge();
+		}, [])
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="title"> age predicter</h1>
+			<h2>Enter a name and ill predict the age :</h2>
+			<input
+				type="text"
+				onChange={event => setName(event.target.value)}
+				value={name}
+			/>
+
+			<button onClick={validateSubmission}> predict age</button>
+			<button onClick={0}></button>
+			<h2 className=" predicted-age">{name} is {age} years old</h2>
+		</>
 	);
 };
 
